f8c1d5be1b084c503058fe772c8ecd437b5e1ea5
Denny Azevedo <denny.filho@animaeducacao.com.br>
2024-03-04 21:30:08 -0300

    :barber: Criando Controller e Model do Usuario :office_worker:

--- eCommerce.API/Models/Usuario.cs
+++ eCommerce.API/Models/Usuario.cs
@@ -2,5 +2,8 @@
 {
 	public class Usuario
 	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string Email { get; set; }
 	}
 }
---
+++ eCommerce.API/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using eCommerce.API.Models;
+
+namespace eCommerce.API.Repositories
+{
+	public interface IUsuarioRepository
+	{
+		public List<Usuario> Get();
+		public Usuario Get(int id);
+		public void Insert(Usuario usuario);
+		public void Update(Usuario usuario);
+		public void Delete(int id);
+	}
+}
